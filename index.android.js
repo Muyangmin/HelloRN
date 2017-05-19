db738653d73a47016a43f8da7cd929625b2e2fc2
Muyangmin <muyangmin@foxmail.com>
2017-05-19 16:01:50 +0800

    A sample of customize class using arbitrary props.

--- index.android.js
+++ index.android.js
@@ -12,23 +12,25 @@ import {
   View
 } from 'react-native';
 
+class Student extends Component {
+    render() {
+        return (
+            <Text style={styles.welcome}>{this.props.name}</Text>
+        );
+    }
+}
+
+
 export default class HelloRN extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Student name="Venus"/>
+                <Student name="Apollo"/>
+                <Student name="Athena"/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
