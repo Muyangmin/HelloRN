5ae5f60689801e0330f0f7a985d5f43e80f4c5b0
Muyangmin <muyangmin@foxmail.com>
2017-05-23 17:25:11 +0800

    Sample for DrawerNavigator.

--- index.android.js
+++ index.android.js
@@ -4,50 +4,98 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Button,
+    Image,
+    StyleSheet,
 } from 'react-native';
 
-export default class HelloRN extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+import {DrawerNavigator} from 'react-navigation';
+
+export default class NewsScreen extends Component {
+    static navigationOptions = {
+        drawerLabel: 'Home',
+        drawerIcon: ({tintColor}) => (
+            <Image
+                source={require('./img/ic_launcher.png')}
+                style={[styles.icon, {tintColor: tintColor}]}
+            />
+        ),
+    };
+
+    render() {
+        return (
+            <Button
+                onPress={() => this.props.navigation.navigate('Profile')}
+                title="View profile"
+            />
+        );
+    }
+}
+
+class ProfileScreen extends Component {
+    static navigationOptions = {
+        drawerLabel: 'Profile',
+        drawerIcon: ({tintColor}) => (
+            <Image source={require("./img/ic_launcher.png")}
+                   style={[styles.icon, {tintColor: tintColor}]}
+            />
+        )
+    };
+
+    render() {
+        return (
+            <Button
+                onPress={() => this.props.navigation.goBack()}
+                title="Go back home"
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    icon: {
+        width: 24,
+        height: 24,
+    },
+});
+
+const HelloRN = DrawerNavigator({ //1st param: Route map
+    News: {
+        screen: NewsScreen,
+    },
+    Profile: {
+        screen: ProfileScreen,
+    },
+}, {
+    //For full API see: https://reactnavigation.org/docs/navigators/drawer#DrawerNavigatorConfig
+    drawerWidth: 200,
+    drawerPosition: 'left',
+    contentOptions: {
+        activeTintColor: '#e91e63',
+        style: {
+            marginVertical: 0,
+        }
+    }
+
 });
 
 AppRegistry.registerComponent('HelloRN', () => HelloRN);
